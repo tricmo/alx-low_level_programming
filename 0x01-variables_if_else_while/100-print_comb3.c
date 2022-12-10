d45a59a37c95631e6178beee95d0434b54002ee7
tricmo <charles.cmolokwu@gmail.com>
2022-12-10 13:12:15 -0800

    Printing double digits with ,

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Program to print double digits separated by ,
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 10)
+		{
+
+			if (j != i && j < i)
+			{
+				putchar('0' + j);
+				putchar('0' + i);
+
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
