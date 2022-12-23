fc6977470ee9165ca8b8a13218d87bfef44fd6ff
tricmo <charles.cmolokwu@gmail.com>
2022-12-23 10:13:03 -0800

    Infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -43,8 +43,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i++;
 	while (*(n2 + j) != '\0')
 		j++;
-		i--;
-		j--;
+	i--;
+	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
@@ -57,7 +57,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-			temp_tot = val1 + val2 + overflow;
+		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
@@ -73,5 +73,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
-		return (r);
+	return (r);
 }
