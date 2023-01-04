8b8aae5b05a00090e51104e284677936b94a60a0
tricmo <charles.cmolokwu@gmail.com>
2023-01-04 15:22:47 -0800

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints string and
+ *  new like standard put
+ * @s: character pointer
+ * Return: Always 0 (Success)
+ */
+void puts_recursion(char *s)
+{
+	if(*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	_putchar('\n');
+}
