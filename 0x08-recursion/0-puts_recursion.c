f6ba30e28b421a0b5e81895019379dfcf526a7c2
tricmo <charles.cmolokwu@gmail.com>
2023-01-04 15:27:40 -0800

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  * @s: character pointer
  * Return: Always 0 (Success)
  */
-void puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	_putchar('\n');
 }
