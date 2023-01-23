4ace6f5bc714aaad731c81cc4ce5b35f9d99a5ac
tricmo <charles.cmolokwu@gmail.com>
2023-01-22 17:55:07 -0800

    Update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,8 +13,12 @@ int sum_them_all(const unsigned int n, ...);
 	unsigned int i, sum = 0;
 	va_list ap;
 
-	for(i = 0; i < n; i++)
+	va_start(ap, n);
+
+	i = 0;
+	while (i < n)
 		sum += va_arg(ap, int);
+		i++;
 	
 	va_end(ap);
 	return (sum);
